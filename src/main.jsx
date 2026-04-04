55a313fca789359c403f55936986b83269fab0aa
SidDaTechieGuyy <kshivasiddharth@gmail.com>
2026-04-04 11:08:02 +0200

    Refactor main.jsx to use React Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import PlasmicHost from "./plasmic-host";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/plasmic-host" element={<PlasmicHost />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
